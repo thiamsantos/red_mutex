da8677f8e25e3297dd516c3166defb66fdb31f18
Thiago Santos <thia.mdossantos@gmail.com>
2019-03-08 13:55:52 -0300

    Remove supervisor

--- mix.exs
+++ mix.exs
@@ -14,8 +14,7 @@ defmodule RedMutex.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger],
-      mod: {RedMutex.Application, []}
+      extra_applications: [:logger]
     ]
   end
 
