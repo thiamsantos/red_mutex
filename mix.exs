7fef4c5d161eacd30c2f0788bd22d4809c7cfbc9
Thiago Santos <thia.mdossantos@gmail.com>
2019-03-10 12:31:57 -0300

    Support elixir 1.5

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule RedMutex.MixProject do
     [
       app: :red_mutex,
       version: "0.1.0",
-      elixir: "~> 1.7",
+      elixir: "~> 1.5",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
